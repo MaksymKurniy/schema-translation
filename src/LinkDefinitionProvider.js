83181e101d39c1afc6e4b99f24496ec7687ea2b4
MaksymKurniy <maksim.k@voodoo-digital.com>
2024-02-14 12:18:43 +0200

    add link support

--- src/LinkDefinitionProvider.js
+++ src/LinkDefinitionProvider.js
@@ -4,63 +4,78 @@ const { log } = require("console");
 
 class LinkDefinitionProvider {
   constructor(pattern, targetTemplate) {
-    this.pattern = pattern;
+    this.regEx = new RegExp(pattern, "g");
     this.targetTemplate = targetTemplate;
-    this.dictionatyJson = JSON.parse(
-      fs.readFileSync(this.targetTemplate, "utf8")
-    );
+    this.lineCache = {};
+    this.pathCache = {};
   }
 
   findValueByPath(jsonPath) {
-    jsonPath = jsonPath.slice(3);
-    jsonPath = jsonPath.slice(0, -1);
-    const fileContent = JSON.stringify(this.dictionatyJson, null, 2);
+    if (this.lineCache[jsonPath]) {
+      return {
+        line: this.lineCache[jsonPath].line,
+        tooltip: this.lineCache[jsonPath].tooltip,
+      };
+    }
 
-    const keys = jsonPath.split(".");
+    let keys = jsonPath.split(".");
+    let keyIdx = 0;
+    let start_line = 0;
+    let lines = this.dictionatyContent.split("\n");
     let tooltip = this.dictionatyJson;
-    let line = -1;
+
     for (const key of keys) {
       tooltip = tooltip[key];
+      if (!tooltip) return { line: -1, tooltip };
+    }
 
-      if (!tooltip) {
-        return { line, tooltip };
+    for (const [key, value] of Object.entries(this.pathCache)) {
+      if (jsonPath.includes(key) && value.line > start_line) {
+        start_line = value.line;
+        keyIdx = value.keyIdx;
       }
     }
-    let keyId = 0;
 
-    const lines = fileContent.split("\n");
-    for (let i = 0; i < lines.length; i++) {
-      if (lines[i].includes('"' + keys[keyId] + '":')) {
-        keyId++;
-        if (keyId == keys.length) {
-          line = i + 1;
-          return { line, tooltip };
+    for (let i = start_line; i < lines.length; i++) {
+      if (lines[i].includes(`"${keys[keyIdx]}":`)) {
+        keyIdx++;
+        let pathLine = keys.slice(0, keyIdx).join(".");
+        if (!this.pathCache[pathLine]) {
+          this.pathCache[pathLine] = { line: i, keyIdx: keyIdx - 1 };
+        }
+
+        if (keyIdx == keys.length) {
+          this.lineCache[jsonPath] = { line: i + 1, tooltip: tooltip };
+          return this.lineCache[jsonPath];
         }
       }
     }
-    return { line, tooltip };
+    return { line: -1, tooltip };
   }
 
   provideDocumentLinks(document) {
-    const regEx = new RegExp(this.pattern, "g");
+    this.dictionatyContent = fs.readFileSync(this.targetTemplate, "utf8");
+    this.dictionatyJson = JSON.parse(this.dictionatyContent);
     const text = document.getText();
     const links = [];
 
     let match;
-    while ((match = regEx.exec(text))) {
-      const startPos = document.positionAt(match.index + 3);
-      const endPos = document.positionAt(match.index + match[0].length - 1);
+    while ((match = this.regEx.exec(text))) {
+      const startPos = document.positionAt(match.index + 3); // slice "t:
+      const endPos = document.positionAt(match.index + match[0].length - 1); // slice "
       const range = new vscode.Range(startPos, endPos);
-      const {line, tooltip} = this.findValueByPath(match[0]);
-      if (line == -1) return;
+      const { line, tooltip } = this.findValueByPath(match[0].slice(3, -1));
+      if (line == -1) continue;
 
       links.push({
         range,
         tooltip: `Translate: "${tooltip}"`,
-        target: vscode.Uri.file(this.targetTemplate).with({fragment: `L${line}`})
+        target: vscode.Uri.from({
+          path: this.targetTemplate,
+          fragment: `L${line}`,
+        }),
       });
     }
-
     return links;
   }
 }
--- src/extension.js
+++ src/extension.js
@@ -1,178 +1,54 @@
-const vscode = require('vscode');
-const fs = require('fs');
-const path = require('path');
-const { log } = require('console');
-const selectOpt = ['label', 'info', 'content']
+const vscode = require("vscode");
+const path = require("path");
+const fs = require("fs");
+const { log } = require("console");
+const selectOpt = ["label", "info", "content"];
 const { LinkDefinitionProvider } = require("./LinkDefinitionProvider");
-
-
-let activeRules = [];
 
 let dictionary = {};
 let schema = {};
 let dictionaryPath = path.join(getWorkFolder(), 'locales', 'en.default.schema.json')
 
 function getDictionaryJson(targetPath) {
-  const jsonData = JSON.parse(fs.readFileSync(dictionaryPath, 'utf8'));
-  return targetPath.split('.').reduce((data, element) => (data && data[element]) ? data[element] : {}, jsonData);
+  const jsonData = JSON.parse(fs.readFileSync(dictionaryPath, "utf8"));
+  return targetPath
+    .split(".")
+    .reduce(
+      (data, element) => (data && data[element] ? data[element] : {}),
+      jsonData
+    );
+}
+
+function getWorkFolder() {
+  const workFolder = vscode.workspace.workspaceFolders;
+  return (!workFolder || workFolder.length === 0)? null : workFolder[0].uri.fsPath;
 }
 
 function findSimilarLabelPath(targetLabel) {
   function recursiveSearch(currentPath, currentValue) {
     if (result) return;
 
-    if (typeof currentValue === 'object') {
+    if (typeof currentValue === "object") {
       for (const [key, value] of Object.entries(currentValue)) {
         const newPath = currentPath ? `${currentPath}.${key}` : key;
         recursiveSearch(newPath, value);
       }
-    } else if (typeof currentValue === 'string' && currentValue.toLowerCase() === targetLabel.toLowerCase()) {
+    } else if (
+      typeof currentValue === "string" &&
+      currentValue.toLowerCase() === targetLabel.toLowerCase()
+    ) {
       result = currentPath;
     }
   }
 
   let result = null;
-  recursiveSearch('', dictionary);
+  recursiveSearch("", dictionary);
   return result;
-}
-
-function replaceInLiquid(inputJson) {
-  try {
-    const editor = vscode.window.activeTextEditor;
-
-    if (!editor) {
-      vscode.window.showErrorMessage('No active document. Exiting.');
-      return;
-    }
-
-    editor.edit(async (editBuilder) => {
-      const document = editor.document;
-      const range  = new vscode.Range(document.positionAt(schema.startIdx), document.positionAt(schema.endIdx));
-      editBuilder.replace(range, '\n' + JSON.stringify(inputJson, null, 2) + '\n');
-    });
-  } catch (error) {
-    console.error(`Error replace: ${error}`);
-  }
-}
-
-function loadLiquidInfo() {
-  const editor = vscode.window.activeTextEditor;
-  if (!editor) {
-    vscode.window.showErrorMessage('No active document. Exiting.');
-    return null;
-  }
-
-  const fileUri = editor.document.uri;
-  const fileName = path.parse(path.basename(fileUri.fsPath)).name;
-
-  const inputJson = extractJsonFromLiquid(fileUri.fsPath);
-
-  if (!inputJson) {
-    vscode.window.showErrorMessage('{% schema %} not found or has errors.');
-  }
-
-  return { inputJson, fileName };
-}
-
-function extractJsonFromLiquid(liquidFilePath) {
-  try {
-    let content = fs.readFileSync(liquidFilePath, 'utf8');
-
-    let start = content.search(/\{%\s*schema\s*%\}/i);
-    schema.endIdx = content.search(/\{%\s*endschema\s*%\}/i);
-
-    if (start !== -1 && schema.endIdx !== -1) {
-      schema.startIdx = start + '{% schema %}'.length;
-      let schemaContent = content.substring(schema.startIdx, schema.endIdx).trim();
-
-      return JSON.parse(schemaContent);
-    }
-  } catch (error) {
-    return null;
-  }
-}
-
-function updateLocale(setting, localePath) {
-  if (typeof setting !== 'object') return;
-
-  const setting_id = setting.id || `${setting.type}__${typeIdx}`;
-  localePath[setting_id] = {};
-
-  for (const [key, value] of Object.entries(setting)) {
-    if (selectOpt.includes(key) && !value.startsWith("t:") && !findSimilarLabelPath(value)) {
-      localePath[setting_id][key] = value;
-    } else if (key === 'options' && Array.isArray(value)) {
-      value.forEach((option, idx) => {
-        if (option.label && !option.label.startsWith("t:") && !option.label.match(/^\d+$/) && !findSimilarLabelPath(option.label)) {
-          localePath[setting_id][`options__${idx}`] = { "label": option.label };
-        }
-      });
-    }
-  }
-}
-
-function updateSchema(setting, path) {
-  if (typeof setting !== 'object') return;
-
-  const setting_id = setting.id || `${setting.type}__${typeIdx}`;
-
-  for (const [key, value] of Object.entries(setting)) {
-    if (selectOpt.includes(key) && !value.startsWith("t:")) {
-      const allPath = findSimilarLabelPath(value);
-      setting[key] = allPath ? `t:sections.all.${allPath}` : `${path}.settings.${setting_id}.${key}`;
-
-      if (!allPath && key === 'content') typeIdx++;
-    } else if (key === 'options' && Array.isArray(value)) {
-      value.forEach((option, idx) => {
-        if (option.label && !option.label.startsWith("t:") && !option.label.match(/^\d+$/)) {
-          const allPath = findSimilarLabelPath(option.label);
-          option.label = allPath ? `t:sections.all.${allPath}` : `${path}.settings.${setting.id}.options__${idx + 1}.label`;
-        }
-      });
-    }
-  }
-}
-
-function genLocale(inputJson, fileName) {
-  typeIdx = 1;
-  if (fileName.includes("section-")) {
-    fileName = fileName.replace("section-", "").replace("main-", "");
-  }
-
-  const translation = { name: {}, settings: {}, blocks: {} };
-
-  if (inputJson.name && !inputJson.name.startsWith("t:")) {
-    translation.name = inputJson.name;
-    inputJson.name = `t:sections.${fileName}.name`;
-
-    if (inputJson.presets && inputJson.presets[0].name) {
-      inputJson.presets[0].name = inputJson.name;
-    }
-  }
-
-  for (const setting of inputJson.settings || []) {
-    updateLocale(setting, translation.settings);
-    updateSchema(setting, `t:sections.${fileName}`);
-  }
-
-  for (const block of (inputJson.blocks || []).filter(b => !b.type.includes('@app'))) {
-    translation.blocks[block.type] = {settings: {}};
-    if (block.name && !block.name.startsWith("t:")) {
-      block.name = `t:sections.${fileName}.blocks.${block.type}.name`;
-    }
-
-    for (const setting of block.settings || []) {
-      updateLocale(setting, translation.blocks[block.type].settings);
-      updateSchema(setting, `t:sections.${fileName}.blocks.${block.type}`);
-    }
-  }
-
-  return { sections: { [fileName]: translation } };
 }
 
 function delEmptyObjects(obj) {
   for (var key in obj) {
-    if (obj[key] && typeof obj[key] === 'object') {
+    if (obj[key] && typeof obj[key] === "object") {
       if (Object.keys(obj[key]).length === 0) {
         delete obj[key];
       } else {
@@ -188,48 +64,157 @@ function delEmptyObjects(obj) {
 
 function replaceInLocale(newLocale) {
   try {
-    const existingLocale = JSON.parse(fs.readFileSync(dictionaryPath, 'utf8'));
+    const existingLocale = JSON.parse(fs.readFileSync(dictionaryPath, "utf8"));
     const updatedTranslations = recursiveAdd(existingLocale, newLocale);
     delEmptyObjects(updatedTranslations);
 
-    fs.writeFileSync(dictionaryPath, JSON.stringify(updatedTranslations, null, 2), 'utf8');
+    fs.writeFileSync(dictionaryPath, JSON.stringify(updatedTranslations, null, 2), "utf8");
   } catch (error) {
     vscode.window.showErrorMessage(`Error adding translations: ${error}`);
   }
 }
 
+function replaceInLiquid(editor, inputJson) {
+  try {
+    editor.edit(async (editBuilder) => {
+      const document = editor.document;
+      const range = new vscode.Range(document.positionAt(schema.startIdx), document.positionAt(schema.endIdx));
+      editBuilder.replace(range, `\n${JSON.stringify(inputJson, null, 2)}\n`);
+    });
+  } catch (error) {
+    console.error(`Error replace: ${error}`);
+  }
+}
+
+function loadLiquidInfo(document) {
+  const fileUri = document.uri.fsPath;
+  const fileName = path.parse(path.basename(fileUri)).name;
+  const inputJson = extractJsonFromLiquid(document);
+
+  if (!inputJson) {
+    vscode.window.showErrorMessage("{% schema %} not found or has errors.");
+  }
+
+  return { inputJson, fileName };
+}
+
+function extractJsonFromLiquid(document) {
+  try {
+    let content = document.getText();
+    let start = content.search(/\{%\s*schema\s*%\}/i);
+    schema.endIdx = content.search(/\{%\s*endschema\s*%\}/i);
+
+    if (start !== -1 && schema.endIdx !== -1) {
+      schema.startIdx = start + '{% schema %}'.length;
+      let schemaContent = content.substring(schema.startIdx, schema.endIdx).trim();
+
+      return JSON.parse(schemaContent);
+    }
+  } catch (error) {
+    return null;
+  }
+}
+
+function updateLocales(setting, translate, path) {
+  if (typeof setting !== "object") return;
+
+  const setting_id = setting.id || `${setting.type}__${typeIdx}`;
+  translate[setting_id] = {};
+
+  for (const [key, value] of Object.entries(setting)) {
+    if (selectOpt.includes(key) && !value.startsWith("t:")) {
+      const allPath = findSimilarLabelPath(value);
+      if (allPath) {
+        setting[key] = `t:sections.all.${allPath}`;
+      } else {
+        translate[setting_id][key] = value;
+        setting[key] = `${path}.settings.${setting_id}.${key}`;
+      }
+      if (!allPath && key === "content") typeIdx++;
+    } else if (key === "options" && Array.isArray(value)) {
+      value.forEach((option, idx) => {
+        if (option.label && !option.label.startsWith("t:") && !option.label.match(/^\d+$/)) {
+          const allPath = findSimilarLabelPath(option.label);
+          if (allPath) {
+            option.label = `t:sections.all.${allPath}`;
+          } else {
+            translate[setting_id][`options__${idx}`] = { label: option.label };
+            option.label = `${path}.settings.${setting.id}.options__${idx + 1}.label`;
+          }
+        }
+      });
+    }
+  }
+}
+
+function genLocale(inputJson, fileName) {
+  typeIdx = 1;
+  const translation = { name: {}, settings: {}, blocks: {} };
+
+  if (fileName.includes("section-")) {
+    fileName = fileName.replace("section-", "").replace("main-", "");
+  }
+
+  if (inputJson.name && !inputJson.name.startsWith("t:")) {
+    translation.name = inputJson.name;
+    inputJson.name = `t:sections.${fileName}.name`;
+
+    if (inputJson.presets && inputJson.presets[0].name) {
+      inputJson.presets[0].name = inputJson.name;
+    }
+  }
+
+  for (const s of inputJson.settings || []) {
+    updateLocales(s, translation.settings, `t:sections.${fileName}`);
+  }
+
+  for (const block of (inputJson.blocks || []).filter(b => !b.type.includes("@app"))) {
+    translation.blocks[block.type] = { name: {}, settings: {} };
+    if (block.name && !block.name.startsWith("t:")) {
+      translation.blocks[block.type].name = block.name;
+      block.name = `t:sections.${fileName}.blocks.${block.type}.name`;
+    }
+
+    for (const s of block.settings || []) {
+      updateLocales(s, translation.blocks[block.type].settings, `t:sections.${fileName}.blocks.${block.type}`);
+    }
+  }
+
+  return { sections: { [fileName]: translation } };
+}
+
 function recursiveAdd(target, source) {
   for (const [key, value] of Object.entries(source)) {
-    if (typeof value === 'object') {
+    if (typeof value === "object") {
       target[key] = recursiveAdd(target[key] || {}, value);
     } else if (!(key in target)) {
       target[key] = value;
     }
   }
+  
   return target;
 }
 
-function getWorkFolder() {
-  const workFolder = vscode.workspace.workspaceFolders;
-  return (!workFolder || workFolder.length === 0)? null : workFolder[0].uri.fsPath;
-}
-
 function translateSchema() {
-  const { inputJson, fileName } = loadLiquidInfo();
+  const editor = vscode.window.activeTextEditor;
+  if (!editor) {
+    vscode.window.showErrorMessage("No active document. Exiting.");
+    return null;
+  }
+  const { inputJson, fileName } = loadLiquidInfo(editor.document);
   if (!inputJson) return;
 
-  dictionary = getDictionaryJson('sections.all');
+  dictionary = getDictionaryJson("sections.all");
 
   replaceInLocale(genLocale(inputJson, fileName));
-  replaceInLiquid(inputJson);
+  replaceInLiquid(editor, inputJson);
 }
 
 function activate(context) {
   let linkProvider = new LinkDefinitionProvider('"t:.*"', dictionaryPath);
 
-  activeRule = vscode.languages.registerDocumentLinkProvider('liquid', linkProvider);
-
-  disposable = vscode.commands.registerCommand('extension.translateSchema', () => translateSchema());
+  activeRule = vscode.languages.registerDocumentLinkProvider("liquid", linkProvider);
+  disposable = vscode.commands.registerCommand("extension.translateSchema", () => translateSchema());
 
   context.subscriptions.push(activeRule);
   context.subscriptions.push(disposable);
@@ -238,5 +223,5 @@ function deactivate() {}
 
 module.exports = {
   activate,
-  deactivate
+  deactivate,
 };
